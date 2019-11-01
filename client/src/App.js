c4d471db28131f8f851184af222cbf9849c70968
Kathleen Leung <kathleenleungky@gmail.com>
2019-11-01 01:40:55 -0700

    added Jumbo & Grid component in App.js layout

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,24 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import Nav from "./components/Nav";
+import { Col, Row, Container } from "./components/Grid";
+import Jumbotron from "./components/Jumbotron";
 
 function App() {
   return (
     <Router>
       <div>
         <Nav />
+        <Container fluid>
+          <Row>
+            <Col size="md-12">
+              <Jumbotron>
+                <h1>(React) Google Books Search</h1>
+                <p>Search for and Save Books of Interest</p>
+              </Jumbotron>
+            </Col>
+          </Row>
+        </Container>
         {/* <Switch>
           <Route exact path="/search" component={Search} />
           <Route exact path="/saved" component={Saved} />
